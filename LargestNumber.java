eff1726c55d38c3e328d5e4bd4c8ab69595cfef7
shapureharshitha <shapureharshithaa@gmail.com>
2026-03-06 20:55:27 +0530

    Add LargestNumber class to find maximum in array

---
+++ LargestNumber.java
@@ -0,0 +1,16 @@
+public class LargestNumber {
+
+    public static void main(String[] args) {
+
+        int arr[] = {5, 2, 9, 1, 7};
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Largest number is: " + max);
+    }
+}
